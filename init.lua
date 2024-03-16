c53a45f0e5435cdc0eb36d3cfaca0c1c9c9e25ac
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-03-16 09:39:49 +0900

    fix: fix cmdheight for rspec.nvim

--- init.lua
+++ init.lua
@@ -85,20 +85,20 @@ return {
 -- set highlight group for any theme
   -- the key is the name of the colorscheme or init
   -- the init key will apply to all colorschemes
-  highlights = {
-    -- apply highlight group to all colorschemes (include the default_theme)
-    init = {
-      -- set the transparency for all of these highlight groups
-      Normal = { bg = "NONE", ctermbg = "NONE" },
-      NormalNC = { bg = "NONE", ctermbg = "NONE" },
-      CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-      CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-      CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
-      LineNr = {},
-      SignColumn = {},
-      StatusLine = {},
-      NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
-      NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
-    },
-  },
+  -- highlights = {
+  --   -- apply highlight group to all colorschemes (include the default_theme)
+  --   init = {
+  --     -- set the transparency for all of these highlight groups
+  --     Normal = { bg = "NONE", ctermbg = "NONE" },
+  --     NormalNC = { bg = "NONE", ctermbg = "NONE" },
+  --     CursorColumn = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  --     CursorLine = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  --     CursorLineNr = { cterm = {}, ctermbg = "NONE", ctermfg = "NONE" },
+  --     LineNr = {},
+  --     SignColumn = {},
+  --     StatusLine = {},
+  --     NeoTreeNormal = { bg = "NONE", ctermbg = "NONE" },
+  --     NeoTreeNormalNC = { bg = "NONE", ctermbg = "NONE" },
+  --   },
+  -- },
 }
--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = false, -- sets vim.opt.wrap
+    cmdheight = 1, -- sets vim.opt.commandheight
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
