90fc4e69d9956f0944836012915c6e0a54f0b93c
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2024-03-24 16:28:30 +0900

    fix: rspec quick fixを表示する

--- plugins/rspec.lua
+++ plugins/rspec.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("rspec").setup {
     -- Whether or not to open the quickfix window when the spec fails.
-    open_quickfix_when_spec_failed = false,
+    open_quickfix_when_spec_failed = true,
     }
   end
 }
